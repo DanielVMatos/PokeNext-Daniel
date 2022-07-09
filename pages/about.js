e386b831b7bbb206c80ad5bb33bb3f73500d07ed
Daniel Vitor <dedomatos12@gmail.com>
2022-07-09 15:59:54 -0300

    corrigindo problema de link externo.

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <div className={styles.about}>
             <h1>Sobre o projeto</h1>
-            <p>Projeto desenvolvido durante as aulas de Next do canal <a href="https://www.youtube.com/c/MatheusBattisti" target="_blank">Matheus Battisti - Hora de Codar</a>. Esse projeto teve como finalidade aprimorar os conhecimentos em Next.</p>
+            <p>Projeto desenvolvido durante as aulas de Next do canal <a href="https://www.youtube.com/c/MatheusBattisti" target={"_blank"} rel={"noreferrer"}>Matheus Battisti - Hora de Codar</a>. Esse projeto teve como finalidade aprimorar os conhecimentos em Next.</p>
             <Image src="/images/charizard.png" alt="Imagem do charizard" width="300px" height="300px" />
         </div>
     )
